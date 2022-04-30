a08bc9bf74dc0120e77525ff6692e879680017d2
raashidkhan1 <raashid.khan1@gmail.com>
2022-04-30 13:16:35 +0200

    Update reload command

--- controller.py
+++ controller.py
@@ -29,8 +29,12 @@ MAX_ECON = 50
 MAX_QCUR = 50
 MIN_QCUR = 10
 
+# src and dst for haproxy
 SRC_HAPROXYCFG = "/haproxy.cfg"
 DST_HAPROXY = "myhaproxy:/etc/haproxy/haproxy.cfg"
+
+# reload haproxy lb command
+RELOAD_HAPROXY_CMD = "podman kill -s HUP myhaproxy"
 
 HEADER_FIELD_NAMES = 'pxname,svname,qcur,qmax,scur,smax,slim,stot,bin,bout,dreq,dresp,ereq,econ,eresp,wretr,wredis,status,weight,act,bck,chkfail,chkdown,lastchg,downtime,qlimit,pid,iid,sid,throttle,lbtot,tracked,type,rate,rate_lim,rate_max,check_status,check_code,check_duration,hrsp_1xx,hrsp_2xx,hrsp_3xx,hrsp_4xx,hrsp_5xx,hrsp_other,hanafail,req_rate,req_rate_max,req_tot,cli_abrt,srv_abrt,comp_in,comp_out,comp_byp,comp_rsp,lastsess,last_chk,last_agt,qtime,ctime,rtime,ttime,agent_status,agent_code,agent_duration,check_desc,agent_desc,check_rise,check_fall,check_health,agent_rise,agent_fall,agent_health,addr,cookie,mode,algo,conn_rate,conn_rate_max,conn_tot,intercepted,dcon,dses,wrew,connect,reuse,cache_lookups,cache_hits,srv_icur,src_ilim,qtime_max,ctime_max,rtime_max,ttime_max,'
 client = PodmanClient(base_url="unix:///run/podman/podman.sock")
@@ -117,8 +121,8 @@ def perform_reset():
     #-------------------------- restart Haproxy
     Event().wait(5)
     #restart haproxy service
-    # reload here --
-    Event.wait(10)
+    os.system(RELOAD_HAPROXY_CMD)
+    Event().wait(10)
 
 
 # function to start stop instances based on metrics from monitorLB
